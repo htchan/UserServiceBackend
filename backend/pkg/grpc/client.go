1ea0988760a06f0a015cfaa37435ae0bd96c8091
htchan <htchanbc@connect.ust.hk>
2021-11-13 18:25:19 +0800

    add constructor in pkg/grpc/client.go to create pb obj

--- backend/pkg/grpc/client.go
+++ backend/pkg/grpc/client.go
@@ -9,3 +9,37 @@ func NewClient(address string) pb.UserServiceClient {
 	conn, _ := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
 	return pb.NewUserServiceClient(conn)
 }
+
+func NewAuthToken(token string) *pb.AuthToken {
+	return &pb.AuthToken{
+		Token: &token,
+	}
+}
+
+func NewServiceName(name string) *pb.ServiceName {
+	return &pb.ServiceName{
+		Name: &name,
+	}
+}
+ 
+func NewAuthorizeParams(token, username, permission string) *pb.AuthorizeParams {
+	return &pb.AuthorizeParams{
+		Token: &token,
+		Username: &username,
+		Permission: &permission,
+	}
+}
+
+func NewLoginParams(username, password string) *pb.LoginParams {
+	return &pb.LoginParams{
+		Username: &username,
+		Password: &password,
+	}
+}
+
+func NewTokenWithPermission(token, permission string) *pb.TokenWithPermission {
+	return &pb.TokenWithPermission{
+		Token: &token,
+		Permission: &permission,
+	}
+}
